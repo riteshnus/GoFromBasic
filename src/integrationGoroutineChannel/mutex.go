0c737175a6af20d36d6225190752ed5865b2fda1
riteshnus <ritesh.nus@gmail.com>
2020-01-31 11:13:10 +0800

    remaining files

---
+++ src/integrationGoroutineChannel/mutex.go
@@ -0,0 +1,211 @@
+/* package main 
+ 
+import ( 
+	"fmt"
+	"runtime" 
+	"os"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(4)
+	
+	f, _ := os.Create("./log.txt")
+	f.Close()
+	
+	logCh := make(chan string, 50)
+	
+	go func() {
+		for {
+			msg, ok := <- logCh
+			if ok {
+				f, _ := os.OpenFile("./log.txt", os.O_APPEND, os.ModeAppend)
+				
+				logTime := time.Now().Format(time.RFC3339)
+				f.WriteString(logTime + " - " + msg)
+				f.Close()
+			} else {
+				break
+			}
+		}
+	}()
+	
+	for i:=1; i < 10;i++ { 
+		for j:=1; j<10;j++ {
+			go func(i, j int) { 
+				msg := fmt.Sprintf("%d + %d = %d\n", i, j, i+j)
+				logCh <- msg
+				fmt.Print(msg)
+			}(i, j)
+		}
+	}
+	
+	fmt.Scanln()
+} */
+
+/* 
+//4.1.1 - initial demo code with sync.Mutex
+//Mutex is method to run the thread under lock and unlock way
+package main 
+ 
+import ( 
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	runtime.GOMAXPROCS(4)
+	
+	mutex := new(sync.Mutex)
+	
+	
+	for i:=1; i < 10;i++ { 
+		for j:=1; j<10;j++ {
+			mutex.Lock()
+			go func() { 
+				fmt.Printf("%d + %d = %d\n", i, j, i+j)
+				mutex.Unlock()
+			}()
+		}
+	}
+	
+	fmt.Scanln()
+} */
+
+
+
+// 2.4.2 - Update to use channel to force sequential access
+// channel replace the mutex lock and unlock
+package main 
+ 
+import (  
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(4)
+	
+	mutex := make(chan bool, 1)
+	
+	
+	for i:=1; i < 10;i++ { 
+		for j:=1; j<10;j++ {
+			mutex <- true
+			go func() { 
+				fmt.Printf("%d + %d = %d\n", i, j, i+j)
+				// mutex <- true
+				<-mutex
+			}()
+			// <-mutex
+		}
+	}
+	
+	fmt.Scanln()
+}
+
+
+/* 
+// 2.4.3 - async logger with channels
+package main 
+ 
+import (  
+	"fmt"
+	"runtime"
+	"os"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(2)
+	f, _ := os.Create("./log.txt")
+	f.Close()
+	
+	logCh := make(chan string, 50)
+	
+	go func() {
+		for {
+			msg, ok := <- logCh
+			if ok {
+				f, _ := os.OpenFile("./log.txt", os.O_RDWR|os.O_APPEND , os.ModeAppend)
+				
+				logTime := time.Now().Format(time.RFC3339)
+				n, err := f.WriteString(logTime + " - " + msg)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println("wrote ", n, " bytes")
+				}
+				f.Close()
+			} else {
+				break
+			}
+		}
+	}()
+	
+	mutex := make(chan bool, 1)
+	
+	 
+	for i:=1; i < 10;i++ { 
+		for j:=1; j<10;j++ {
+			mutex <- true
+			go func() { 
+				msg := fmt.Sprintf("%d + %d = %d\n", i, j, i+j)
+				logCh <- msg
+				fmt.Print(msg)
+				<-mutex
+			}()
+		}
+	}
+	
+	fmt.Scanln()
+}
+ */
+
+/* 2.4.4 - remove mutex and pass parameters into goroutine
+package main 
+ 
+import ( 
+	"fmt"
+	"runtime" 
+	"os"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(4)
+	
+	f, _ := os.Create("./log.txt")
+	f.Close()
+	
+	logCh := make(chan string, 50)
+	
+	go func() {
+		for {
+			msg, ok := <- logCh
+			if ok {
+				f, _ := os.OpenFile("./log.txt", os.O_APPEND, os.ModeAppend)
+				
+				logTime := time.Now().Format(time.RFC3339)
+				f.WriteString(logTime + " - " + msg)
+				f.Close()
+			} else {
+				break
+			}
+		}
+	}()
+	
+	for i:=1; i < 10;i++ { 
+		for j:=1; j<10;j++ {
+			go func(i, j int) { 
+				msg := fmt.Sprintf("%d + %d = %d\n", i, j, i+j)
+				logCh <- msg
+				fmt.Print(msg)
+			}(i, j)
+		}
+	}
+	
+	fmt.Scanln()
+}
+*/
